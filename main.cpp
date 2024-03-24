95c268f8313e2f60c6b24c99d1e7a93cdeaf73cb
grishinak <st122261@student.spbu.ru>
2024-03-24 12:48:14 +0300

    + - * / trough value and << overdrive

--- main.cpp
+++ main.cpp
@@ -7,35 +7,75 @@ template<typename T = uint32_t>
 class OnesComplement{
     private:
     
-T value=0 ;
+T value = 0 ;
 std::bitset<(sizeof(T)*8)> binaryRepresent;
     public:
 
 OnesComplement(T valueInput){
     if (valueInput>=0){
     value=valueInput;
-    std::cout<<"created obj with value: "<<value;
+    //std::cout<<"created obj with value: "<<value;
 
     binaryRepresent=value;
-    std::cout<<"\t binary represent: "<<binaryRepresent<<std::endl;
+   // std::cout<<"\t binary represent: "<<binaryRepresent<<std::endl;
     } else{
     value=valueInput;
-    std::cout<<"created obj with value: "<<value;
+    //std::cout<<"created obj with value: "<<value;
 
     binaryRepresent=abs(value);
     binaryRepresent.flip();
-    std::cout<<"\t binary represent: "<<binaryRepresent<<std::endl;
+   // std::cout<<"\t binary represent: "<<binaryRepresent<<std::endl;
     } 
 }
+    // Перегрузка оператора сложения
+    OnesComplement operator+(const OnesComplement& other) const {
+        T resultValue = value + other.value;
+        return OnesComplement(resultValue);
+    }
 
-     OnesComplement operator+()
-    {}
+    // Перегрузка оператора вычитания
+    OnesComplement operator-(const OnesComplement& other) const {
+        T resultValue = value - other.value;
+        return OnesComplement(resultValue);
+    }
+
+    // Перегрузка оператора умножения
+    OnesComplement operator*(const OnesComplement& other) const {
+        T resultValue = value * other.value;
+        return OnesComplement(resultValue);
+    }
+
+    // Перегрузка оператора деления
+    OnesComplement operator/(const OnesComplement& other) const {
+        T resultValue = value / other.value;
+        return OnesComplement(resultValue);
+    }
+
+    // Перегрузка оператора вывода <<
+    friend std::ostream& operator<<(std::ostream& os, const OnesComplement& oc) {
+        os << "value: \t" << oc.value << "\t binary represent: " << oc.binaryRepresent;
+        return os;
+    }
 };
 
-int main(){
+int main() {
+    OnesComplement<int> a(-12);
+    OnesComplement<int> b(5);
 
-    OnesComplement<int> a (-1);
-    OnesComplement<int> b (1);
+    std::cout << "a: \t\t" << a << std::endl;
+    std::cout << "b: \t\t" << b << std::endl;
+
+    OnesComplement<int> sum = a + b;
+    std::cout << "Sum: \t\t" << sum << std::endl;
+
+    OnesComplement<int> difference = a - b;
+    std::cout << "Difference: \t" << difference << std::endl;
+
+    OnesComplement<int> product = a * b;
+    std::cout << "Product: \t" << product << std::endl;
+
+    OnesComplement<int> quotient = a / b;
+    std::cout << "Quotient: \t" << quotient << std::endl;
 
     return 0;
 }
