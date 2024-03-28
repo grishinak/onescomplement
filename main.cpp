a5a434eb471f6e0124e51dc2b81832b378c20ddd
grishinak <st122261@student.spbu.ru>
2024-03-28 13:29:09 +0300

    saving a representation in array

--- main.cpp
+++ main.cpp
@@ -1,39 +1,8 @@
-#include <iostream>
-#include <cstdint>
+#include "onescomplement.h"
 
-template<typename T = uint32_t>
-class OnesComplement {
-private:   
-    T value = 0;
-    T binaryRepresent[sizeof(T)*8]; // Массив для хранения битового представления числа
-
-public:
-    OnesComplement(T valueInput) {
-        if (valueInput >= 0) {
-            value = valueInput;
-            for (int i = 0; i < sizeof(T)*8; ++i) {
-                binaryRepresent[i] = (value >> i) & 1; // Заполнение массива битов
-            }
-        } else {
-            value = valueInput;
-            T absValue = -value; // Получение абсолютного значения для беззнаковых типов данных
-            for (int i = 0; i < sizeof(T)*8; ++i) {
-                binaryRepresent[i] = !((absValue >> i) & 1); // Заполнение массива инвертированным битовым представлением
-            }
-        } 
-    }
-    
-    void printBinaryRepresentation() {
-        std::cout << "Binary Representation: ";
-        for (int i = sizeof(T)*8 - 1; i >= 0; --i) {
-            std::cout << static_cast<int>(binaryRepresent[i]);
-        }
-        std::cout << std::endl;
-    }
-};
 
 int main() {
-    OnesComplement<int> onesComp(-5);
+    OnesComplement<int> onesComp(5);
     onesComp.printBinaryRepresentation();
     return 0;
 }
--- onescomplement.h
+++ onescomplement.h
@@ -1,57 +1,38 @@
-// #include <iostream>
-// #include <cstdint>
-// #include <bitset>
-// #include <cmath>
+#ifndef ONESCOMPLEMENT_H
+#define ONESCOMPLEMENT_H
 
-// template<typename T = uint32_t>
-// class OnesComplement{
+#include <iostream>
+#include <cstdint>
 
-// private:   
-// T value = 0 ;
-// std::bitset<(sizeof(T)*8)> binaryRepresent;
+template<typename T = uint32_t>
+class OnesComplement {
+private:   
+    T value = 0;
+    T binaryRepresent[sizeof(T)*8]; // Массив для хранения битового представления числа
 
-// public:
-// OnesComplement(T valueInput){
-// if (valueInput>=0){
-// value=valueInput;
+public:
+    OnesComplement(T valueInput) {
+        if (valueInput >= 0) {
+            value = valueInput;
+            for (int i = 0; i < sizeof(T)*8; ++i) {
+                binaryRepresent[i] = (value >> i) & 1; // Заполнение массива битов
+            }
+        } else {
+            value = valueInput;
+            T absValue = -value; // Получение абсолютного значения для беззнаковых типов данных
+            for (int i = 0; i < sizeof(T)*8; ++i) {
+                binaryRepresent[i] = !((absValue >> i) & 1); // Заполнение массива инвертированным битовым представлением
+            }
+        } 
+    }
+    
+    void printBinaryRepresentation() {
+        std::cout << "Binary Representation: ";
+        for (int i = sizeof(T)*8 - 1; i >= 0; --i) {
+            std::cout << static_cast<int>(binaryRepresent[i]);
+        }
+        std::cout << std::endl;
+    }
+};
 
-// binaryRepresent=value;
-// } else{
-// value=valueInput;
-
-// binaryRepresent=abs(value);
-// binaryRepresent.flip();
-// } 
-
-// }
-
-// // Перегрузка оператора сложения
-// OnesComplement operator+(const OnesComplement& other) const {
-//     T resultValue = value + other.value;
-//     return OnesComplement(resultValue);
-// }
-
-// // Перегрузка оператора вычитания
-// OnesComplement operator-(const OnesComplement& other) const {
-//     T resultValue = value - other.value;
-//     return OnesComplement(resultValue);
-// }
-
-// // Перегрузка оператора умножения
-// OnesComplement operator*(const OnesComplement& other) const {
-//     T resultValue = value * other.value;
-//     return OnesComplement(resultValue);
-// }
-
-// // Перегрузка оператора деления
-// OnesComplement operator/(const OnesComplement& other) const {
-//     T resultValue = value / other.value;
-//     return OnesComplement(resultValue);
-// }
-
-// // Перегрузка оператора вывода <<
-// friend std::ostream& operator<<(std::ostream& os, const OnesComplement& oc) {
-//     os << "value: \t" << oc.value << "\t binary represent: " << oc.binaryRepresent;
-//     return os;
-// }
-// };
+#endif //ONESCOMPLEMENT_H
