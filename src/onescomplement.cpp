3b367576b99e05e845df18d73a54959d2b824fcb
grishinak <st122261@student.spbu.ru>
2024-03-28 16:36:29 +0300

    class division to cpp and h files

---
+++ src/onescomplement.cpp
@@ -0,0 +1,59 @@
+#include "onescomplement.h"
+
+template<typename T>
+OnesComplement<T>::OnesComplement(T valueInput){
+if (valueInput>=0){
+value=valueInput;
+
+binaryRepresent=value;
+} else {
+value=valueInput;
+
+binaryRepresent=abs(value);
+binaryRepresent.flip();
+}
+}
+
+
+// Перегрузка оператора сложения
+template<typename T>
+OnesComplement<T> OnesComplement<T>::operator+(const OnesComplement& other) const {
+    T resultValue = value + other.value;
+    return OnesComplement<T>(resultValue);
+}
+
+// Перегрузка оператора вычитания
+template<typename T>
+OnesComplement<T> OnesComplement<T>::operator-(const OnesComplement& other) const {
+    T resultValue = value - other.value;
+    return OnesComplement(resultValue);
+}
+
+// Перегрузка оператора умножения
+template<typename T>
+OnesComplement<T> OnesComplement<T>::operator*(const OnesComplement& other) const {
+    T resultValue = value * other.value;
+    return OnesComplement(resultValue);
+}
+
+// Перегрузка оператора деления
+template<typename T>
+OnesComplement<T> OnesComplement<T>::operator/(const OnesComplement& other) const {
+    T resultValue = value / other.value;
+    return OnesComplement(resultValue);
+}
+
+// Перегрузка оператора <<
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const OnesComplement<T>& oc) {
+    os << "value: \t" << oc.value << "\t binary represent: " << oc.binaryRepresent;
+    return os;
+}
+
+//Явная инстанциация оператора << , для используемых в программе типов данных для шаблона
+template std::ostream& operator<<(std::ostream& os, const OnesComplement<uint32_t>& oc);
+template std::ostream& operator<<(std::ostream& os, const OnesComplement<int>& oc);
+
+//Явная инстанциация класса, для используемых в программе типов данных для шаблона
+template class OnesComplement<uint32_t>;
+template class OnesComplement<int>;
